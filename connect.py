27285e44097b2056dcf0cb27ffa87edc40347a4d
iTubePod <hmgc_mauro@icloud.com>
2019-04-10 16:42:45 -0500

    Final

--- connect.py
+++ connect.py
@@ -1,40 +1,21 @@
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
-
-def add_friend(tx, name, friend_name):
-    tx.run("MERGE (a:Person {name: $name}) "
-           "MERGE (a)-[:KNOWS]->(friend:Person {name: $friend_name})",
-           name=name, friend_name=friend_name)
-
-def print_friends(tx, name):
-    for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
-                         "RETURN friend.name ORDER BY friend.name", name=name):
-        print(record["friend.name"])
+driver = GraphDatabase.driver("bolt://localhost:7687")
 
 def get_top_contributors(tx):
     print("Top 10 contributors")
-    for record in tx.run("MATCH (g:Node)-[relationship:RESPONDED]->(n:Node) "
-                        "RETURN g.userID, count(relationship) AS responses ORDER BY responses DESC "
+    for record in tx.run("MATCH (g)-[relationship:AnsweredTo]->(n) "
+                        "RETURN g.userId, count(relationship) AS responses ORDER BY responses DESC "
                         "LIMIT 10"):
-        print(record["g.userID"])
+        print(record["g.userId"])
 
 def last_active_users(tx):
     print("Last 10 active users")
-    for record in tx.run("MATCH (g:Node)-[relationship:RESPONDED]->(n:Node) "
-                        "RETURN g.userID, relationship.timestamp ORDER BY relationship.timestamp DESC "
-                        "LIMIT 100"):
-        print(record["g.userID"])
-
-def path_between_users(u_id1, u_id2):
-    print("The shortest path between user {u_id1} and user {u_id2} is "
-    for record in tx.run("MATCH (user1:Node { userID:$u_id1 }),(user2:Node { userID:$u_id2 }), "
-                        "p = shortestPath((user1)-[*..5]-(user2))"
-                        "RETURN length(p)", u_id1=u_id1, u_id2=u_id2):
-        print(record["friend.name"])
+    for record in tx.run("MATCH (g)-[relationship:AnsweredTo]->(n) "
+                        "RETURN g.userId, relationship.timestamp ORDER BY relationship.timestamp DESC "
+                        "LIMIT 10"):
+        print(record["g.userId"])
 
 with driver.session() as session:
-    session.write_transaction(add_friend, "Arthur", "Guinevere")
-    session.write_transaction(add_friend, "Arthur", "Lancelot")
-    session.write_transaction(add_friend, "Arthur", "Merlin")
-    session.read_transaction(print_friends, "Arthur")
+    session.read_transaction(get_top_contributors)
+    session.read_transaction(last_active_users)
